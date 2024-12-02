d7b4b65a1ef323e0a9b6f9943f424aa0c1ab7b46
phann <phann@Anthony>
2024-12-01 21:44:50 -0700

    BSTreeNode

--- assignment3StartingCode/src/implementations/BSTreeNode.java
+++ assignment3StartingCode/src/implementations/BSTreeNode.java
@@ -1,5 +1,41 @@
 package implementations;
 
 public class BSTreeNode<E> {
+	private E element;
+	private BSTreeNode<E> left, right;
+
+	public BSTreeNode(E elem, BSTreeNode<E> left, BSTreeNode<E> right) {
+		this.element = elem;
+		this.left = left;
+		this.right = right;
+	}
+	
+	public BSTreeNode(E elem){
+		this.element = elem;				
+		}
+
+	public E getElement() {
+		return element;
+	}
+
+	public BSTreeNode<E> getLeft() {
+		return left;
+	}
+
+	public BSTreeNode<E> getRight() {
+		return right;
+	}
+
+	public void setElement(E element) {
+		this.element = element;
+	}
+
+	public void setLeft(BSTreeNode<E> left) {
+		this.left = left;
+	}
+
+	public void setNext(BSTreeNode<E> right) {
+		this.right = right;
+	}
 
 }
